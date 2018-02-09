39fef411ae9c60c012925c1e22a8ecbe2b240e22
Jan Bílek <honza@elisiondesign.cz>
2018-02-10 00:47:10 +0100

    initial commit

---
+++ book_flight.py
@@ -0,0 +1,8 @@
+import json
+import requests
+
+api_flights_url = "https://api.skypicker.com/flights"
+api_booking_url = "http://128.199.48.38:8080/booking"
+
+print (api_booking_url)
+print (api_flights_url)
