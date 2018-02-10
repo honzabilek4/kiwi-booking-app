16c5a27f4dc2bc1becc1641811232f3b9b712465
Jan Bílek <honza@elisiondesign.cz>
2018-02-10 14:02:57 +0100

    Linting fixes

--- book_flight.py
+++ book_flight.py
@@ -2,8 +2,8 @@ import json
 import requests
 import argparse
 
-api_flights_url = "https://api.skypicker.com/flights"
-api_booking_url = "http://128.199.48.38:8080/booking"
+api_flights_url = 'https://api.skypicker.com/flights'
+api_booking_url = 'http://128.199.48.38:8080/booking'
 
 parser = argparse.ArgumentParser(
     description='Book the cheapest or fastest flight between two destinations using Kiwi public api.')
@@ -14,7 +14,7 @@ parser.add_argument('--to', type=str, required=True, help='To destination in IAT
 parser.add_argument('--bags', type=int, default=1, help='Number of bags to be added. (default is one)')
 
 flight_type_group = parser.add_mutually_exclusive_group()
-flight_type_group.add_argument('--one-way', action='store_true',default=True, help='Book one way ticket. (default)')
+flight_type_group.add_argument('--one-way', action='store_true', default=True, help='Book one way ticket. (default)')
 flight_type_group.add_argument('--return', type=int, help='Number of days to stay.')           
 
 search_type_group = parser.add_mutually_exclusive_group()
