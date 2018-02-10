7bdcf53987929d11b112808f99a02be609d44d41
Jan Bílek <honza@elisiondesign.cz>
2018-02-10 14:00:19 +0100

    Add CLI arguments

--- book_flight.py
+++ book_flight.py
@@ -1,8 +1,28 @@
 import json
 import requests
+import argparse
 
 api_flights_url = "https://api.skypicker.com/flights"
 api_booking_url = "http://128.199.48.38:8080/booking"
 
-print (api_booking_url)
-print (api_flights_url)
+parser = argparse.ArgumentParser(
+    description='Book the cheapest or fastest flight between two destinations using Kiwi public api.')
+
+parser.add_argument('--date', type=str, required=True, help='Date of the flight in format yyyy-mm-dd.')
+parser.add_argument('--from', type=str, required=True, help='From destination in IATA code format')
+parser.add_argument('--to', type=str, required=True, help='To destination in IATA code format') 
+parser.add_argument('--bags', type=int, default=1, help='Number of bags to be added. (default is one)')
+
+flight_type_group = parser.add_mutually_exclusive_group()
+flight_type_group.add_argument('--one-way', action='store_true',default=True, help='Book one way ticket. (default)')
+flight_type_group.add_argument('--return', type=int, help='Number of days to stay.')           
+
+search_type_group = parser.add_mutually_exclusive_group()
+search_type_group.add_argument('--cheapest', action='store_true', default=True, 
+                    help='Book cheapest flight. (default)')
+search_type_group.add_argument('--fastest', action='store_true', default=False,
+                    help='Book the shortest flight between destinations.')
+
+
+args = parser.parse_args()
+print(args)
